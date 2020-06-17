ac61ec4dd07ec2c366c3cc7362672fc0fbae1695
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2020-06-18 02:36:18 +0300

    feat(styles): use NormalizeCSS in the global `App` (theme) wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Normalize } from 'styled-normalize';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '~src/theme';
 
 const App = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 App.propTypes = {
